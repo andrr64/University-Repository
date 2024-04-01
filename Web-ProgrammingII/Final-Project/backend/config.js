8c559f184cab8e2bbab1b33ec8e5b19a5d2f7f59
andrr64 <derzandrr03@gmail.com>
2024-04-02 03:45:20 +0700

    set mongodb url

--- Web-ProgrammingII/Final-Project/backend/config.js
+++ Web-ProgrammingII/Final-Project/backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 9999;
+
+const _mongoDBPassword = 'ANDrea5!MONgodBroot';
+export const mongoDBURL = 
+    `mongodb+srv://root:${_mongoDBPassword}@cluster0.esrllss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
