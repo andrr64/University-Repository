4935ab4e311b707c7424b1f51644654cdfe5c14d
andrr64 <derzandrr03@gmail.com>
2024-04-02 03:33:50 +0700

    set listen and root route

--- Web-ProgrammingII/Final-Project/backend/index.js
+++ Web-ProgrammingII/Final-Project/backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import {PORT} from "./config.js"
 
 const app = express();
+// HTTP Routes
 
+// app.get($route, $callbackFunction(req, resp))
+
+// This is main/root route
+app.get('/',(request, response) =>{
+    console.log(request);
+    return response.status(234).send('Welcome to the MERN tutorial')
+});
+
+app.listen(PORT, () =>{
+    console.log(`App is listening to port: ${PORT}`);
+});
