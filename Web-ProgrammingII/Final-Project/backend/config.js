4c5543f68efa7a469bbbd9f85b7910f2b45f59c2
andrr64 <derzandrr03@gmail.com>
2024-04-02 12:12:57 +0700

    set mongodb url to local database

--- Web-ProgrammingII/Final-Project/backend/config.js
+++ Web-ProgrammingII/Final-Project/backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-const _mongoDBPassword = 'ANDrea5!MONgodBroot';
 export const mongoDBURL = 
-    `mongodb+srv://root:${_mongoDBPassword}@cluster0.esrllss.mongodb.net/self-database`;
-    // self-database adalah nama database yang akan dihubungkan
+    `mongodb://localhost:27017/books-project`;
+    // books-projec adalah nama database yang akan dihubungkan
+    // ngomong-ngomong ini koneksi lokal ya...
