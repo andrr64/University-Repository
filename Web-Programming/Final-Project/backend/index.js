33a97f2c6b0e761652c9d5ff979abb188bfc25fd
andrr64 <derzandrr03@gmail.com>
2024-04-03 15:04:46 +0700

    delete Final-Project

--- Web-Programming/Final-Project/backend/index.js
+++ Web-Programming/Final-Project/backend/index.js
@@ -10,15 +10,15 @@ const app = express();
 app.use(express.json()); // Middleware for parsing request body
 // Middleware for handling CORS policy
 // 1. Allow All origins with default cors
-// app.use(cors());
+app.use(cors());
 // 2. Allow specific origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type']
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// );
 
 // HTTP Routes
 // app.get($route, $callbackFunction(req, resp))
