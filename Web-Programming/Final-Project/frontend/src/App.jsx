d56458234c4ba30f74e59a2c30bf7a6e9790b0f1
andrr64 <derzandrr03@gmail.com>
2024-04-03 23:49:31 +0700

    update Routes

--- Web-Programming/Final-Project/frontend/src/App.jsx
+++ Web-Programming/Final-Project/frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import CreateBook from "./pages/CreateBook";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/detail/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
