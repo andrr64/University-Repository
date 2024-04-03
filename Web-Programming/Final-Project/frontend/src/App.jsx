1276afc3d7125e030308dd8cd4b4c1f1a38245a0
andrr64 <derzandrr03@gmail.com>
2024-04-04 00:19:52 +0700

    update App.jsx

--- Web-Programming/Final-Project/frontend/src/App.jsx
+++ Web-Programming/Final-Project/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ShowBook from "./pages/ShowBook";
 import DeleteBook from "./pages/DeleteBook";
