286fd99980fcde718165054013387eca7dceafd5
andrr64 <derzandrr03@gmail.com>
2024-04-08 10:11:46 +0700

    create test API route

--- Web-Programming/MERN-RealEstate/api/index.js
+++ Web-Programming/MERN-RealEstate/api/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js';
 
 dotenv.config();
 
@@ -14,6 +15,15 @@ mongoose
     });
 
 const app = express(); // Express Object
-app.listen(process.env.PORT, () => {
+
+app.listen(3000, () => {
     console.log(`Server berhasil berjalan...`);
 })
+
+app.get('/', (request, response) => {
+    const nama = 'Andreas';
+    response.send(`<h1>Hai ${nama}, Selamat datang di Facebook</h1>`);
+    // response.json({message: 'Hello World'})
+})
+
+app.use('/api/user', userRoute); 
