3aa4103987864c4706c3f612786078c4d0e5ca8e
andrr64 <derzandrr03@gmail.com>
2024-04-09 04:19:40 +0700

    create middleware and a function to handle error

--- Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
+++ Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
@@ -1,17 +1,17 @@
-import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
 
     const newUser = new User({username, email, password: bcryptjs.hashSync(password, 10)})
 
-    await newUser.save(); //Menyimpan data ke server mongodb ke server yang terhubung di index.js
     try {
         await newUser.save();
         res.status(201).json('User created successfully');
     } catch (error) {
-        res.status(500).json(error.message);
+        next(errorHandler(550, 'error from internal server`'));
     }
 }
 
