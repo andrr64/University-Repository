9f57c269d125b84e77de3bafc28346d321443c78
andrr64 <derzandrr03@gmail.com>
2024-04-09 20:45:53 +0700

    create API: sign-in route

--- Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
+++ Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-const signup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
 
     const newUser = new User({username, email, password: bcryptjs.hashSync(password, 10)})
@@ -15,4 +16,28 @@ const signup = async (req, res, next) => {
     }
 }
 
-export default signup;
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email}); // Temukan pengguna dengan email tertentu
+        if (!validUser) return next(errorHandler(404, 'User not found!')); // Jika tidak ditemukan maka kirim kode 404
+        
+        // Validator Password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        // Jika tidak valid maka kirim kode 401
+        if (!validPassword) return next(errorHandler(401, 'Wrong crendentials!'));
+        
+        // Mendapatkan token
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+
+        // Memisahkan data 'password' dan kirim ke user'
+        const {password: pass, ...rest} = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest); 
+ 
+    } catch (error) {
+        next(error);
+    }
+}
