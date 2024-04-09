eb79aacc0663d60c1a748e99594c9f741178e494
andrr64 <derzandrr03@gmail.com>
2024-04-09 20:53:12 +0700

    update comment

--- Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
+++ Web-Programming/MERN-RealEstate/api/controllers/auth.controller.js
@@ -17,26 +17,27 @@ export const signup = async (req, res, next) => {
 }
 
 export const signin = async(req, res, next) => {
+    //1. Destruktur data dari request client
     const {email, password} = req.body;
     try {
-        const validUser = await User.findOne({email}); // Temukan pengguna dengan email tertentu
-        if (!validUser) return next(errorHandler(404, 'User not found!')); // Jika tidak ditemukan maka kirim kode 404
-        
-        // Validator Password
+        // 2. Cari apakah ada user dengan email yang sama
+        const validUser = await User.findOne({email});
+            // 2a. Jika tidak ada atau user tidak ditemukan, maka kirim response: 404, User not found
+        if (!validUser) return next(errorHandler(404, 'User not found!'));        
+        // 3. Validasi password dari client dengan database
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        // Jika tidak valid maka kirim kode 401
+            // 3a. Jika tidak sama/salah password maka kirim response: 401, Wrong credentials 
         if (!validPassword) return next(errorHandler(401, 'Wrong crendentials!'));
         
-        // Mendapatkan token
+        // 4. Membuat token
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
 
-        // Memisahkan data 'password' dan kirim ke user'
+        // 5. Memisahkan data 'password' lalu kirim data yang sudah bersih ke client
         const {password: pass, ...rest} = validUser._doc;
         res
             .cookie('access_token', token, { httpOnly: true })
             .status(200)
             .json(rest); 
- 
     } catch (error) {
         next(error);
     }
