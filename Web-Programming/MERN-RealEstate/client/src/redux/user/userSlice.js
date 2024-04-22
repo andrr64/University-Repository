a96b82a810e7b9de4d5f202642e3b13f3d5d9c30
andrr64 <derzandrr03@gmail.com>
2024-04-23 01:16:11 +0700

    menambah komentar

--- Web-Programming/MERN-RealEstate/client/src/redux/user/userSlice.js
+++ Web-Programming/MERN-RealEstate/client/src/redux/user/userSlice.js
@@ -13,11 +13,25 @@ const userSlice = createSlice({
         signInStart: (state) => {
             state.loading = true;
         },
+        /*
+        user/signInSuccess
+        params:
+            - state: variabel data
+            - action: berisi data akun
+        function: mengubah nilai state
+        */
         signInSuccess: (state, action) => {
             state.currentUser = action.payload;
             state.loading = false;
             state.error = null;
         },
+        /*
+        user/signInFailure
+        params:
+            - state: variabel data
+            - action: berisi pesan error
+        function: mengubah nilai error menjadi action.payload atau pesan error serta mengubah satte loading menjadi false
+        */
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
@@ -25,5 +39,5 @@ const userSlice = createSlice({
     }
 })
 
-export const { signInStart, signInFailure, signInSuccess } = userSlice.actions;
+export const { signInStart, signInFailure, signInSuccess} = userSlice.actions;
 export default userSlice.reducer;
